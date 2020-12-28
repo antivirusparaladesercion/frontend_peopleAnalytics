f3b293171231e309d673bd5b0b81181c4a5c9c72
arcangel <saio8812@hotmail.com>
2020-12-28 00:06:51 -0500

    login logaut auth0 con certificados, componente visualizar archi. cargados ok

---
+++ src/views/upload/UploadView/DownloadFiles.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import axios from 'axios';
+import {
+  Box,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TablePagination,
+  TableRow,
+  Typography,
+  makeStyles,
+  Link,
+  LinearProgress,
+  CardHeader,
+  CardContent,
+  Divider
+} from '@material-ui/core';
+
+
+
+const useStyles = makeStyles(theme => ({
+  root: {},
+  avatar: {
+    marginRight: theme.spacing(2)
+  }
+}));
+
+const DownloadFiles = ({ className, prefi, ...rest }) => {
+  console.log('el prefix enviado', prefi)
+  const classes = useStyles();
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+
+  let [responseData, setResponseData] = useState('');
+
+  
+
+
+  const fetchData = async () => {
+    
+    await axios({
+      method: 'post',
+      url:
+        'https://x7t8iwhc2e.execute-api.us-east-1.amazonaws.com/getUrlsUploaded',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      data: {
+        nombre_ies: prefi
+      }
+    })
+      .then(res => {
+        setResponseData(res.data.data);
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleLimitChange = event => {
+    setLimit(event.target.value);
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setPage(newPage);
+  };
+ console.log('la responsedata',responseData);
+  return (
+    <Card className={clsx(classes.root, className)} {...rest}>
+      {responseData === '' ? (
+        <>
+          <CardHeader
+            subheader="los archivos que ha cargado podrán ser visualizados en breve."
+            title="Cargando Archivos Enviados"
+          />
+          <Divider />
+          <CardContent>
+            <LinearProgress />
+            <LinearProgress color="secondary" />
+          </CardContent>
+        </>
+      ) : (
+          <>
+            <CardHeader
+              subheader="Estos son los archivos que se han cargado al sistema para ser analizados."
+              title="Archivos Cargados"
+            />
+            <Divider />
+            <CardContent>
+              <PerfectScrollbar>
+                <Box minWidth={1050}>
+                  <Table>
+                    <TableHead>
+                      <TableRow>
+                        <TableCell>Archivo</TableCell>
+                        <TableCell>Link de descarga</TableCell>
+                        <TableCell>Fecha de carga del archivo</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {responseData.slice(0, limit).map(link => (
+                        <TableRow hover key={link.date}>
+                          <TableCell>
+                            <Box alignItems="center" display="flex">
+                              <Typography color="textPrimary" variant="body1">
+                                {link.file_name}
+                              </Typography>
+                            </Box>
+                          </TableCell>
+                          <TableCell>
+                            <Box alignItems="center" display="flex">
+                              <Typography color="textPrimary" variant="body1">
+                                <Link href={link.url}>Descargar</Link>
+                              </Typography>
+                            </Box>
+                          </TableCell>
+                          <TableCell>
+                            <Box alignItems="center" display="flex">
+                              <Typography color="textPrimary" variant="body1">
+                                {link.date}
+                              </Typography>
+                            </Box>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </Box>
+              </PerfectScrollbar>
+              <TablePagination
+                component="div"
+                count={responseData.length}
+                onChangePage={handlePageChange}
+                onChangeRowsPerPage={handleLimitChange}
+                page={page}
+                rowsPerPage={limit}
+                rowsPerPageOptions={[5, 10, 25]}
+              />
+            </CardContent>
+          </>
+        )}
+    </Card>
+  );
+};
+
+DownloadFiles.propTypes = {
+  className: PropTypes.string,
+  customers: PropTypes.array.isRequired
+};
+
+export default DownloadFiles;
--- src/views/upload/UploadView/index.js
+++ src/views/upload/UploadView/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import { Container, makeStyles } from '@material-ui/core';
 import Page from 'src/components/Page';
 import Upload from './Upload';
-import { withAuthenticationRequired } from "@auth0/auth0-react";
+import DownloadFiles from './DownloadFiles';
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,13 +17,74 @@ const useStyles = makeStyles(theme => ({
 function UploadView() {
   const classes = useStyles();
 
-  return (
+  //prueba dinamica
+
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+
+
+  const [userMetadata, setUserMetadata] = useState(null);
+
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = process.env.REACT_APP_DOMAIN;
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          audience: `https://${domain}/api/v2/`,
+          scope: "read:current_user",
+        });
+
+        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
+
+        const metadataResponse = await fetch(userDetailsByIdUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { user_metadata } = await metadataResponse.json();
+
+        setUserMetadata(user_metadata);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, []);
+
+  console.log('uuser: ', user)
+  let prefijo='';
+  if (userMetadata) {
+    prefijo = userMetadata.u_prefix;
+    console.log('prueba adentro prefix_u arriba download: ');
+  }
+  
+  
+  
+
+  
+
+
+  //
+
+  return prefijo.length >0 && (
     <Page className={classes.root} title="Upload Files">
       <Container maxWidth={false}>
 
-        <Upload/>
+        <Upload userMetadata={userMetadata} prefi={prefijo}/>
 
       </Container>
+      
+
+      <Container maxWidth={false}>
+
+        <DownloadFiles prefi={prefijo}/>
+
+      </Container>
+
     </Page>
   );
 }
